584e401d132a2b9b397852a1688536340b8f8dde
Jonathan Berkhahn <jaberkha@us.ibm.com>
2020-02-10 14:44:54 -0800

    List based solution for weekdays and specific holidays

--- calendar.go
+++ calendar.go
@@ -3,12 +3,36 @@ package calendar
 import "github.com/jberkhahn/automated_testing/pkg/date"
 
 type HolidayCalendar struct {
+	weekdayHolidays  []string
+	specificHolidays []date.Date
 }
 
-func (c HolidayCalendar) SetWeekdayHoliday(DayOfTheWeek string) {
-
+func (c *HolidayCalendar) SetWeekdayHoliday(DayOfTheWeek string) {
+	c.weekdayHolidays = append(c.weekdayHolidays, DayOfTheWeek)
 }
 
-func (c HolidayCalendar) IsHoliday(date date.Date) bool {
+func (c *HolidayCalendar) SetDateHoliday(year, month, day int) {
+	c.specificHolidays = append(c.specificHolidays, date.NewDate(year, month, day))
+}
+
+func (c *HolidayCalendar) IsHoliday(inputDate date.Date) bool {
+	return c.isWeekdayHoliday(inputDate) || c.isSpecificHoliday(inputDate)
+}
+
+func (c *HolidayCalendar) isWeekdayHoliday(inputDate date.Date) bool {
+	for _, v := range c.weekdayHolidays {
+		if v == inputDate.Weekday() {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *HolidayCalendar) isSpecificHoliday(inputDate date.Date) bool {
+	for _, v := range c.specificHolidays {
+		if v == inputDate {
+			return true
+		}
+	}
 	return false
 }
--- calendar_test.go
+++ calendar_test.go
@@ -16,3 +16,39 @@ func TestWeekdayHoliday(t *testing.T) {
 		t.Errorf("Saturday should be a holiday")
 	}
 }
+
+func TestWeekdayNotHoliday(t *testing.T) {
+	c := HolidayCalendar{}
+	d := date.NewDate(2020, 6, 1)
+
+	if c.IsHoliday(d) == true {
+		t.Errorf("Monday should not be a holiday")
+	}
+}
+
+func TestMultipleWeekdayHolidays(t *testing.T) {
+	c := HolidayCalendar{}
+	d := date.NewDate(2020, 6, 6)
+	d2 := date.NewDate(2020, 6, 7)
+
+	c.SetWeekdayHoliday("Saturday")
+	c.SetWeekdayHoliday("Sunday")
+
+	if c.IsHoliday(d) != true {
+		t.Errorf("Saturday should be a holiday")
+	}
+	if c.IsHoliday(d2) != true {
+		t.Errorf("Sunday should be a holiday")
+	}
+}
+
+func TestSpecificHoliday(t *testing.T) {
+	c := HolidayCalendar{}
+	d := date.NewDate(2020, 1, 1)
+
+	c.SetDateHoliday(2020, 1, 1)
+
+	if c.IsHoliday(d) != true {
+		t.Errorf("New Years Day should be a holiday")
+	}
+}
